a16623a55896dbc6c7323fcc895b4fcf75a5ca5d
AleNerima <ale95polioni@gmail.com>
2025-01-02 09:31:03 +0100

    Push con classe feedback creata con getter and setter

--- src/entities/feedback/Feedback.java
+++ src/entities/feedback/Feedback.java
@@ -1,4 +1,42 @@
 package entities.feedback;
 
 public class Feedback {
+
+    private int id;
+    private String titolo;
+    private String testo;
+    private int punteggio;
+    //decommenta quando verrà creata
+
+    //private Prenotazione prenotazione;
+
+
+
+    //Getter and Setter
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public int getPunteggio() {
+        return punteggio;
+    }
+
+    public void setPunteggio(int punteggio) {
+        this.punteggio = punteggio;
+    }
+
+    public String getTesto() {
+        return testo;
+    }
+
+    public void setTesto(String testo) {
+        this.testo = testo;
+    }
+
+
 }
