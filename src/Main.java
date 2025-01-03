e099b47327a6f7592d0727717b4e148801cfd1e8
Francesco Rosati <rosati.francesco1993@gmail.com>
2025-01-03 11:42:27 +0100

    Versione 2.1.0 - (Fatti 3 metodi su 7, per ora i primi 2 funzionano)

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import entities.user.Host;
 import entities.user.User;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class Main
 {
@@ -24,16 +25,39 @@ public class Main
                 host.getHostCode()             // Codice host
         );
 
+        Abitazione abitazione1 = new Abitazione(
+                "Appartamento Roma",       // Nome
+                "Via dei Tulipani 32",            // Indirizzo
+                5,                             // Numero locali
+                8,                             // Numero posti letto
+                1,                             // Piano
+                host.getHostCode()             // Codice host
+        );
+
+        Abitazione abitazione2 = new Abitazione(
+                "Appartamento Milano",       // Nome
+                "Via dei Papaveri 45",            // Indirizzo
+                2,                             // Numero locali
+                4,                             // Numero posti letto
+                5,                             // Piano
+                host.getHostCode()             // Codice host
+        );
+
         // Creazione di una prenotazione
-        LocalDate dataInizioDisp = LocalDate.of(2025, 1, 1);
-        LocalDate dataFineDisp = LocalDate.of(2025, 1, 30);
+        LocalDate dataInizioDisp = LocalDate.of(2024, 11, 1);
+        LocalDate dataFineDisp = LocalDate.of(2024, 12, 30);
 
 
         abitazione.aggiungiDisponibilita(dataInizioDisp, dataFineDisp);
 
         Database.addAbitazione(abitazione);
+        Database.addAbitazione(abitazione1);
+        Database.addAbitazione(abitazione2);
 
         host.getAbitazioni().add(abitazione);
+        host.getAbitazioni().add(abitazione1);
+        host.getAbitazioni().add(abitazione2);
+
 
         User user = new User("Giulia", "Verdi", "giulia.verdi@email.com", "Via Appia 20");
         Database.addUtente(user);
@@ -42,15 +66,41 @@ public class Main
         Database.addUtente(user2);
 
         // Creazione di una prenotazione
-        LocalDate dataInizio = LocalDate.of(2025, 1, 10);
-        LocalDate dataFine = LocalDate.of(2025, 1, 15);
+        LocalDate dataInizio = LocalDate.of(2024, 12, 1);
+        LocalDate dataFine = LocalDate.of(2024, 12, 3);
 
         System.out.println(Prenotazione.creaPrenotazione(user, abitazione, dataInizio, dataFine));
 
-        LocalDate dataInizio1 = LocalDate.of(2025, 1, 11);
-        LocalDate dataFine1 = LocalDate.of(2025, 1, 14);
+        LocalDate dataInizio1 = LocalDate.of(2024, 11, 25);
+        LocalDate dataFine1 = LocalDate.of(2024, 11, 30);
 
         System.out.println(Prenotazione.creaPrenotazione(user2, abitazione, dataInizio1, dataFine1));
+
+
+        List<Abitazione> abitazioniHostMario = Database.getAbitazioniHost(host.getHostCode());
+
+        for(Abitazione a: abitazioniHostMario)
+        {
+            System.out.println(a.getNome());
+        }
+
+
+        Prenotazione ultimaPrenotazioneGiulia = Database.getUltimaPronotazioneUtente(user.getId());
+
+        System.out.println("L'ultima prenotazione dell'utente " + user.getName() + " con id " + user.getId() + " è la prenotazione che inizia il " + ultimaPrenotazioneGiulia.getDataInizio());
+
+
+        // Ottenere l'abitazione più gettonata nell'ultimo mese
+        Abitazione abitazionePiuGettonata = Database.getAbitazionePiùGettonataUltimoMese();
+
+        if (abitazionePiuGettonata != null) {
+            System.out.println("L'abitazione più gettonata nell'ultimo mese è:");
+            System.out.println("Nome: " + abitazionePiuGettonata.getNome());
+            System.out.println("Indirizzo: " + abitazionePiuGettonata.getIndirizzo());
+            System.out.println("Numero posti letto: " + abitazionePiuGettonata.getNumeroPostiLetto());
+        } else {
+            System.out.println("Nessuna abitazione con prenotazioni nell'ultimo mese.");
+        }
 
     }
 
--- src/database/Database.java
+++ src/database/Database.java
@@ -1,60 +1,88 @@
 package database;
 
+import entities.feedback.Feedback;
 import entities.property.Abitazione;
 import entities.property.Prenotazione;
 import entities.user.Host;
 import entities.user.User;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-public class Database
-{
+public class Database {
     private static Map<Integer, User> users = new HashMap<>();
     private static Map<Integer, Host> hosts = new HashMap<>();
     private static Map<Integer, Abitazione> abitazioni = new HashMap<>();
     private static Map<Integer, Prenotazione> prenotazioni = new HashMap<>();
 
     // Aggiunta User
-    public static void addUtente(User user)
-    {
+    public static void addUtente(User user) {
         users.put(user.getId(), user);
         System.out.println("Hai inserito l'utente " + user.getName() + " " + user.getSurname());
     }
 
     // Aggiunta Host
-    public static void addHost(Host host)
-    {
+    public static void addHost(Host host) {
         hosts.put(host.getId(), host);
-        System.out.println("Hai inserito l'host " + host.getName() + " " + host.getSurname() + " Codice host: " + host.getHostCode())  ;
+        System.out.println("Hai inserito l'host " + host.getName() + " " + host.getSurname() + " Codice host: " + host.getHostCode());
     }
 
     // Aggiunta Abitazione
-    public static void addAbitazione(Abitazione abitazione)
-    {
+    public static void addAbitazione(Abitazione abitazione) {
         abitazioni.put(abitazione.getId(), abitazione);
         System.out.println("Hai inserito l'abitazione " + abitazione.getNome() + ", indirizzo: " + abitazione.getIndirizzo() + ", Codice host: " + abitazione.getCodiceHost());
     }
 
     // Aggiunta Prenotazione (Verifica delle prenotazioni sovrapposte)
-    public static void addPrenotazione(Prenotazione prenotazione)
-    {
+    public static void addPrenotazione(Prenotazione prenotazione) {
         prenotazioni.put(prenotazione.getId(), prenotazione);
         System.out.println("Hai inserito la prenotazione");
     }
 
-    // Aggiunta Feedback
-
-
 
     //1. Ottenere le abitazioni corrispondenti a un certo codice host
+    public static List<Abitazione> getAbitazioniHost(String codiceHost) {
+        List<Abitazione> list_abitazioni = new ArrayList<>();
 
+        for (Abitazione abitazione : abitazioni.values()) {
+            if (abitazione.getCodiceHost().equals(codiceHost)) {
+                list_abitazioni.add(abitazione);
+            }
+        }
+
+        return list_abitazioni;
+    }
 
     //2. Ottenere l'ultima prenotazione dato un id utente
+    public static Prenotazione getUltimaPronotazioneUtente(Integer idUtente) {
+        User utente = users.get(idUtente);
 
+        if (utente != null && utente.getPrenotazioni() != null && !utente.getPrenotazioni().isEmpty()) {
+            return utente.getPrenotazioni().get(utente.getPrenotazioni().size() - 1);
+        }
+
+        return null;
+    }
 
     //3. Ottenere l'abitazione più gettonata nell'ultimo mese
+    public static Abitazione getAbitazionePiùGettonataUltimoMese() {
+        LocalDate oggi = LocalDate.now();
+        LocalDate ultimaData = oggi.minusMonths(1);
 
+        Map<Abitazione, Long> prenotazioniPerAbitazione = prenotazioni.values().stream()
+                .filter(p -> !p.getDataFine().isBefore(ultimaData)) // Filtra le prenotazioni dell'ultimo mese
+                .collect(Collectors.groupingBy(Prenotazione::getAbitazione, Collectors.counting()));
+
+        return prenotazioniPerAbitazione.entrySet().stream()
+                .max(Map.Entry.comparingByValue()) // Trova l'entrata con il massimo valore
+                .map(Map.Entry::getKey)
+                .orElse(null);
+
+    }
 
     //4. Ottenere gli host con più prenotazioni nell'ultimo mese
 
